4395c1e7b4c09ac736a9007fc8d5afa6c046cfbb
Kyungmin Kim <gaval@Kyungmins-MacBook-Pro.local>
2020-07-26 17:22:30 +0900

    initial commit

---
+++ src/main/java/org/gaval/spring/Application.java
@@ -0,0 +1,14 @@
+package org.gaval.spring;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class Application{
+
+	public static void main(String [] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
---
+++ src/main/java/org/gaval/spring/datajpa/repository/TutorialRepository.java
@@ -0,0 +1,13 @@
+package org.gaval.spring.datajpa.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.gaval.spring.datajpa.model.Tutorial;
+
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+	List<Tutorial> findByPublished(boolean publisehd);
+	List<Tutorial> findByTitleContaining(String title);
+}
+
